ef4fb8e4812d210f60963cbf6c5277c8ce27b20f
Carlos <souzacef@gmail.com>
2024-07-12 15:39:01 -0300

    Função para cadastrar notas.

--- index.js
+++ index.js
@@ -5,29 +5,57 @@ console.log("==========Bem vindo ao controle de notas!==========\n");
 const aluno = prompt("informe o nome do aluno. ");
 
 const materias = [];
+function inserirMateria() {
+    console.log();
+    materias.push(prompt("Qual matéria deseja adicionar? "));
+    console.log();
+}
+
 function cadastrarMaterias() {
     let cont = 0;
     let continuarExecutando = true;
     let quantidadeMaterias = materias.length;
     for (let i = 0; i <3; i++){
-        materias.push(+prompt("Qual matéria deseja adicionar? "));
+        inserirMateria();
     }
 
     while (continuarExecutando) {
         quantidadeMaterias = materias.length;
-        console.log(`${quantidadeMaterias} matérias adicionadas `);
+        console.log(`\n${quantidadeMaterias} matérias adicionadas `);
         console.log("Deseja continuar adicionando matérias? ");
-        let resposta = prompt("Digite 1 para sim, 2 para não. ")
+        let resposta = +prompt("Digite 1 para sim, 2 para não. ")
 
         if (resposta == 1) {
-            materias.push(+prompt("Qual matéria deseja adicionar? "));
+            inserirMateria();
         }else {
-            console.log("Cadastro encerrado");
-            console.log("Próxima etapa");
+            console.log("\nCadastro encerrado");
+            console.log("Próxima etapa.\n");
             continuarExecutando = false;
         }
     }
 
 }
 
+
+function cadastrarNotas() {
+    const notas = {};
+
+    materias.forEach((materia, cont) => {
+        notas[materia] = [];
+        console.log(`\n=====Cadastro de notas para ${materia}=====\n`);
+
+        for (let i = 0; i < 3; i++) {
+            let nota = parseFloat(+prompt(`Digite a ${i + 1}ª nota para ${materia} `));
+            notas[materia].push(nota);
+        }
+    });
+
+    console.log("\nNotas cadastradas!\n");
+    
+
+}
+
+
+
 cadastrarMaterias();
+cadastrarNotas();
